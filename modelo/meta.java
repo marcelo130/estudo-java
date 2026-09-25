117d4e025f4d783723abdc6c232b6151b4764aae
marcelo130 <marcelo.gruber@escola.pr.gov.br>
2026-09-25 07:40:50 -0300

    adicionando meta

---
+++ modelo/meta.java
@@ -0,0 +1,12 @@
+package model;            
+            public class Meta {
+                
+            // atributos//
+            
+             private String objetivo ;
+             private double valorAlvo ;
+             private double valorPoupado;
+                
+                
+             System.out.printtn ("deseja poupar R$ 200,00 para meta [comprar computador]?"                                                              )   
+            }
